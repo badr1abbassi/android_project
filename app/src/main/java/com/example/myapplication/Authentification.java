fa2b7d8dd16bda70ecd5e04f708c486299410f61
GhitaELAMLAQUI <ghita_elamlaqui@um5.ac.ma>
2020-05-08 08:01:50 +0000

    Interface Fils

--- app/src/main/java/com/example/myapplication/Authentification.java
+++ app/src/main/java/com/example/myapplication/Authentification.java
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,11 +20,16 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
-public class Authentification extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Authentification extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     EditText editTextemail , editTextmdp;
     ProgressBar progressBar;
     private FirebaseAuth mAuth;
+    Spinner spinner ;
+    int choix;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,21 +38,40 @@ public class Authentification extends AppCompatActivity implements View.OnClickL
         findViewById(R.id.textViewAuth).setOnClickListener(this);
         findViewById(R.id.btnAuth).setOnClickListener(this);
         findViewById(R.id.progressBar);
+
         editTextmdp = findViewById(R.id.mdp);
         editTextemail = findViewById(R.id.email);
         mAuth=FirebaseAuth.getInstance();
+        spinner = findViewById(R.id.spinRole);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+       ArrayAdapter<CharSequence> spinAdapter = ArrayAdapter.createFromResource(this,R.array.role,android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+       spinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+       spinner.setAdapter(spinAdapter);
+       //specify the interface implementation
+       spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
     }
-
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        choix = position;
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        String error = " Choisir un role";
+        Toast.makeText(getApplicationContext(),error, Toast.LENGTH_SHORT).show();
+    }
     private void authentification() {
         String email = editTextemail.getText().toString().trim();
         String mdp = editTextmdp.getText().toString().trim();
+
         if(email.isEmpty()){
             editTextemail.setError("Vous devez entrer votre email");
             editTextemail.requestFocus();
             return;
         }
         if(mdp.isEmpty()){
-            editTextmdp.setError("Vous devez entrerle mot de passe");
+            editTextmdp.setError("Vous devez entrer votre mot de passe");
             editTextmdp.requestFocus();
             return;
         }
@@ -52,10 +80,21 @@ public class Authentification extends AppCompatActivity implements View.OnClickL
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()) {
-                    Intent intent = new Intent(Authentification.this, MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
+                    //si spinner est le pere
+                    if (choix == 0) {
+                        Intent intent = new Intent(Authentification.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                    //si spinner est le fil => se diriger vers avctivite fils
+                    else {
+                        Intent intent = new Intent(Authentification.this, FilsActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+
                 } else {
+
                     Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
 
@@ -75,4 +114,5 @@ public class Authentification extends AppCompatActivity implements View.OnClickL
                 break;
         }
     }
+
 }
---
+++ app/src/main/java/com/example/myapplication/FilsActivity.java
@@ -0,0 +1,245 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class FilsActivity extends AppCompatActivity {
+
+    Button localisation,camera,appel,sante,taches,aider;
+    private static final int REQUEST_LOCATION = 1;
+    LocationManager locationManager;
+    String lattitude,longitude;
+    private static final int REQUEST_CALL = 1;
+    private static final String number = "+212625733640";
+
+    private FusedLocationProviderClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fils);
+
+
+        localisation=findViewById(R.id.buttonLocalisation);
+        sante=findViewById(R.id.buttonHealth);
+        camera=findViewById(R.id.buttonCamera);
+        appel = findViewById(R.id.buttonAppel);
+        taches = findViewById(R.id.buttonTaches);
+
+        sante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myClick(v);
+            }
+        });
+
+        localisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myClick(v);
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myClick(v);
+            }
+        });
+
+        appel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myClick(v);
+            }
+        });
+
+        taches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myClick(v);
+            }
+        });
+
+
+    }
+
+    private	void	myClick(View	v) {
+        switch (v.getId()){
+            case R.id.buttonLocalisation:
+                getPosition(v);
+                break;
+            case R.id.buttonHealth:
+                getAlarm(v);
+                break;
+            case R.id.buttonCamera:
+                getScanner(v);
+                break;
+            case R.id.buttonAppel:
+                getAppel(v);
+                break;
+            case R.id.buttonTaches:
+                getTache(v);
+                break;
+            default:
+                Toast.makeText(this, v.getId()+"", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void getPosition2(View v){
+        getPermissionLocalisation();
+        //if(ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED) {
+        client.getLastLocation().addOnSuccessListener(FilsActivity.this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    Toast.makeText(FilsActivity.this, "localisaton:" + location, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(FilsActivity.this, "localisaton: NULL", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        /*}else{
+            Toast.makeText(MainActivity.this, "localisaton: permission", Toast.LENGTH_SHORT).show();
+            getPermissionLocalisation();
+        }*/
+    }
+    public void getPermissionLocalisation(){
+        ActivityCompat.requestPermissions(FilsActivity.this, new String[]{ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+    }
+
+
+
+    public void getPosition(View view) {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            getLocation();
+        }
+    }
+    public  void getAlarm(View v){
+        Intent intent =new Intent(this,Alarm.class);
+        this.startActivity(intent);
+
+    }
+    public  void getScanner(View v){
+        Intent intent =new Intent(this,Scanner.class);
+        this.startActivity(intent);
+
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(FilsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (FilsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(FilsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (location != null) {
+                double latti = location.getLatitude();
+                double longi = location.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+            } else if (location1 != null) {
+                double latti = location1.getLatitude();
+                double longi = location1.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+
+            } else if (location2 != null) {
+                double latti = location2.getLatitude();
+                double longi = location2.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+
+
+            } else {
+                Toast.makeText(this, "Unble to Trace your location", Toast.LENGTH_SHORT).show();
+
+            }
+            if(longitude!=null && lattitude !=null){
+                Toast.makeText(this, "Your current location is" + "\n" + "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude, Toast.LENGTH_SHORT).show();
+                Intent intent =new Intent(this,MapsActivity.class);
+                intent.putExtra("longitude",longitude);
+                intent.putExtra("lattitude",lattitude);
+                startActivity(intent);
+            }else{
+                Toast.makeText(this, "makayn walo", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    protected void buildAlertMessageNoGps() {
+
+        Toast.makeText(this, "activer gps", Toast.LENGTH_SHORT).show();
+    }
+
+    // MENU
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.dec) {
+            FirebaseAuth.getInstance().signOut();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getAppel(View v){
+        if (ContextCompat.checkSelfPermission(FilsActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(FilsActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            String dial = "tel:" + number;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+    public void getTache(View v){
+        Intent intent =new Intent(this, TacheActivity.class);
+        this.startActivity(intent);
+    }
+
+}
+
